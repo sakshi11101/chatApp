54e58beaa5ba2d6f766ca83e9881141de8c310d3
sakshi11101 <sakshi11101@gmail.com>
2021-07-15 15:37:23 +0530

    started working on avatar and uploaded and downloaded avatar

--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/storage';
 
 const config = {
   apiKey: 'AIzaSyCBfj5WuWvi6TRVNfrZ_OIWC0yn_eEAmQw',
@@ -15,3 +16,4 @@ const app = firebase.initializeApp(config);
 
 export const auth = app.auth();
 export const database = app.database();
+export const storage = app.storage();
