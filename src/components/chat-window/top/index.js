99a06d2a4be89a7e4bab0701c98804b260920730
sakshi11101 <sakshi11101@gmail.com>
2021-07-19 11:57:57 +0530

    added edit room drawer

--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { useCurrentRoom } from '../../../context/current-room.context';
 import { useMediaQuery } from '../../../misc/custom-hooks';
 import RoomInfoBtnModal from './RoomInfoBtnModal';
+import EditRoomBtnDrawer from './EditRoomBtnDrawer';
 
 const Top = () => {
   const name = useCurrentRoom(v => v.name);
@@ -28,7 +29,9 @@ const Top = () => {
           <span className="text-disappear">{name}</span>
         </h4>
 
-        <ButtonToolbar className="white-space: no-wrap">todo</ButtonToolbar>
+        <ButtonToolbar className="white-space: no-wrap">
+          <EditRoomBtnDrawer />
+        </ButtonToolbar>
       </div>
 
       <div className="d-flex justify-content-between align-items-center">
